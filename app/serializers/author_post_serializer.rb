68fa43aa3ca7f613d1a13a1426960a25d639b533
Michael Lobman <michael.lobman@gmail.com>
2022-02-16 15:52:40 -0500

    Complete assignment

---
+++ app/serializers/author_post_serializer.rb
@@ -0,0 +1,9 @@
+class AuthorPostSerializer < ActiveModel::Serializer
+  attributes :title, :short_content, :tags
+
+  belongs_to :author
+
+  def short_content
+    "#{self.object.content[0..39]}..."
+  end
+end
